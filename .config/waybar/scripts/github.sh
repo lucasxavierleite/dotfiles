562324143c9479827b7989ff4bd1fc0c195dc478
Lucas Xavier Leite <lucasxavierleite@pm.me>
2023-05-18 00:43:25 -0300

    Update waybar and sway configs

--- .config/waybar/scripts/github.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-token=`cat ${HOME}/.config/github/notifications.token`
-count=`curl -u lucas:${token} https://api.github.com/notifications | jq '. | length'`
-tooltip=`echo $count`
-
-if [[ "$count" != "0" ]]; then
-    echo '{"text":'$count',"tooltip":'$tooltip',"class":"$class"}'
-fi
